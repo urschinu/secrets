adc5eb9b44906c9da5aff5b368a973c4da2d9ebf
Your Name <you@example.com>
2023-12-09 15:06:39 +0530

    2nd commit

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ userSchema.pre('save', function (next) {
         bcrypt.hash(user.password, salt, function (err, hash) {
             if (err) return next(err);            
             user.password = hash;
-            console.log(hash);
+            
             next();
         });
     });
