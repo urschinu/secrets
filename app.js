541a24b2236a1c99d24e5866d9234383fba8bd35
Your Name <you@example.com>
2023-12-09 15:03:51 +0530

    add env vars

---
+++ app.js
@@ -0,0 +1,102 @@
+// jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// Use bcrypt to hash passwords
+userSchema.pre('save', function (next) {
+    const user = this;
+    
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);            
+            user.password = hash;
+            console.log(hash);
+            next();
+        });
+    });
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(() => {
+            res.render("secrets");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Error saving user");
+        });
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        // Handle incorrect username or password
+                        res.render("login", { errorMessage: "Incorrect username or password" });
+                    }
+                });
+            } else {
+                // Handle incorrect username or password
+                res.render("login", { errorMessage: "Incorrect username or password" });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Error finding user");
+        });
+});
+
+app.listen(3000, function () {
+    console.log('listening on port 3000.');
+});
